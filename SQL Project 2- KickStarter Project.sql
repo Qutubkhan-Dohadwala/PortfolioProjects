5a61b895423af93b6d8af671a60a3cfee2c4d880
Qutubkhan-Dohadwala <139137443+Qutubkhan-Dohadwala@users.noreply.github.com>
2023-08-07 06:00:47 +0530

    Sql Project 2- KickStarter Project.sql

--- SQL Project 2- KickStarter Project.sql
+++ SQL Project 2- KickStarter Project.sql
@@ -1,19 +1,63 @@
-﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# SQL Server Management Studio Solution File, Format Version 19.00
-VisualStudioVersion = 15.0.28307.421
-MinimumVisualStudioVersion = 10.0.40219.1
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Default|Default = Default|Default
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{B668E673-6485-4C8E-B06E-B8EA8F81B35B}.Default|Default.ActiveCfg = Default
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {B84A57EE-5FEB-40D9-AEF2-0F62EE61DDC4}
-	EndGlobalSection
-EndGlobal
+﻿-----
+Skills used : Case Statement, Temp Tables, Rank Function, Aggregate Functions, Date Function
+
+-----
+Select *
+From [Portfolio Project 2].. kickstarter_projects
+
+-- Projects in which Category were most likely to reach their goals
+
+Select category,
+       count(id) as Success
+From [Portfolio Project 2].. kickstarter_projects
+where State = 'Successful'
+group by category
+order by count(id) desc
+
+-- Did the dollar amount required have an impact on which goals were accomplished most often
+Select country,
+sum(Case When goal < 1000 and State = 'Successful' then 1 else 0 end) as 'Successes when Goal less than 1000' ,
+sum(Case When goal between 1000 and 10000 and State = 'Successful' then 1 else 0 end) as 'Successes when Goal between 1000 and 10000',
+sum(Case When goal > 10000 and State= 'Successful' then 1 else 0 end) as 'Successes when Goal greater than 10000'
+From [Portfolio Project 2].. kickstarter_projects
+group by country
+
+-- Did the projects which were successful belong to different categories over the years
+
+with info as (
+       Select rank() over( partition by datepart(year, deadline) order by count(id) desc) as 'Rank',
+       datepart(year, deadline) as Year,
+       category,
+       count(id) as Success
+From [Portfolio Project 2].. kickstarter_projects
+where State = 'Successful'
+group by category, datepart(year, deadline) 
+)
+
+select *
+from info
+where rank = 1
+
+-- Percentage of Projects successful in Music 
+with music_info as (
+    Select
+     sum(Case When Category = 'Music' and State = 'Successful' then 1 else 0 end) as 'Successful_Music_Ventures',
+     Sum(Case when Category = 'Music' then 1 else 0 end) as 'Total_Music_Projects'
+from [Portfolio Project 2].. kickstarter_projects
+)
+
+Select
+(Successful_Music_Ventures*100/Total_Music_Projects)  as 'Percent Success'
+from music_info
+
+-- Percentage of Projects successful in Films
+with film_info as (
+    Select
+     sum(Case When Category = 'Film & Video' and State = 'Successful' then 1 else 0 end) as 'Successful_Film_Ventures',
+     Sum(Case when Category = 'Film & Video' then 1 else 0 end) as 'Total_Film_Projects'
+from [Portfolio Project 2].. kickstarter_projects
+)
+
+Select
+(Successful_Film_Ventures*100/Total_Film_Projects)  as 'Percent Success'
+from film_info
